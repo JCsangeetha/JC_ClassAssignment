f3367df45344de540cc5d4933ea243eb40a6629b
JCsangeetha <Sangeetha.jc7@outlook.com>
2025-12-19 13:17:55 +0530

    Add files via upload
    Week1 class activity assignments

---
+++ week1/day1/HelloTestLeaf.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	
+	//Write Java code to print “Welcome to TestLeaf”.
+	public static void main(String[]args) 
+	{
+		System.out.println("Welcome to TestLeaf");
+	}
+
+}
