d40265149850ac5861c0881e748c494117911a9e
JCsangeetha <Sangeetha.jc7@outlook.com>
2025-12-29 17:05:46 +0530

    Add files via upload
    week 1 class activity assignment

---
+++ week1/day2/CheckNumberIsPositive.java
@@ -0,0 +1,24 @@
+package week1.day2;
+
+public class CheckNumberIsPositive {
+
+	/*Assignment Details:
+Create a class named CheckNumberIsPositive with integer variable as version with value 10
+-Write a logic to print the given number Positive or Negative. */
+	
+	public static void main(String[]args) 
+	{
+		int version = 10;
+		
+		if(version>0) 
+		{
+		System.out.println("The given number is positive");
+		}
+		
+		else 
+		{
+			System.out.println("The given number is negative");
+		}
+	}
+	
+}
